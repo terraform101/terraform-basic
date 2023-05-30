862eddbcd5a8adfd09573caf36420e09946b217d
Great-Stone <hahohh@gmail.com>
2023-05-30 15:13:34 +0900

    fix local name

--- 3-44.tf
+++ 3-44.tf
@@ -4,14 +4,9 @@ variable "prefix" {
 
 locals {
   name    = "terraform"
-  content = "${var.prefix} ${local.name}"
-  my_info = {
-    age    = 20
-    region = "KR"
-  }
-  my_nums = [1, 2, 3, 4, 5]
 }
 
-locals {
-  content = "content2" # 중복 선언되었으므로 오류가 발생한다.
+resource "local_file" "abc" {
+  content  = local.content
+  filename = "${path.module}/abc.txt"
 }
